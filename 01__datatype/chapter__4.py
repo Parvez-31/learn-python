8f08e2ea74b459ae5fe39287162a872726922fa0
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2025-09-14 12:43:43 +0530

    learn global variables

---
+++ 01__datatype/chapter__4.py
@@ -0,0 +1,16 @@
+# x = 5
+# y = "parvez"
+# print(x + y)
+
+
+x = "programming language"
+
+
+def myfunc():
+    x = "fantastic"
+    print("python is a " + x)
+
+
+myfunc()
+
+print("python is a " + x)
