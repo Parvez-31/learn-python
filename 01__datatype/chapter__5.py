329572f35b4f34e37c72f9a45fd9889a4d92e1bc
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2025-09-16 22:46:27 +0530

    learn slice

---
+++ 01__datatype/chapter__5.py
@@ -0,0 +1,22 @@
+import random
+
+# x = 56
+# y = 45
+# print(x + y)
+
+# Arithmetic operators
+
+# Operator Precedence
+
+print((6 + 3) - (6 + 3))
+print(100 + 5 * 3)
+print(5 + 4 - 7 + 3)
+
+x = 3 + 5j
+print(type(x))
+
+
+print(random.randrange(1, 10))
+
+b = "hello world"
+print(b[:])
